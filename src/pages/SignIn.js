382d2d4ea947975fd4a65b0fc8f1334a09cd28ec
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-02-02 10:59:01 +0100

    Frondend2 auth en login connected to backend

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -37,7 +37,7 @@ function SignIn() {
     event.preventDefault();
 
     try {
-      const response = await axios.post(`https://polar-lake-14365.herokuapp.com/api/auth/signin`, {
+      const response = await axios.post(`http://localhost:8080/api/auth/signin`, {
         username: username,
         password: password,
       })
